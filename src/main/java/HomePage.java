7bbc980a991cf5fb1db78bd287bfa9fc66511cb5
tetianakravchuk <tetiana.kravchukqa@gmail.com>
2022-01-24 18:45:40 -0500

    Initial commit

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage extends BaseAction{
+    public HomePage(WebDriver driver, WebDriverWait wait) {
+
+    }
+}
---
+++ src/test/java/Create.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Create {
+    WebDriver driver;
+    WebDriverWait wait;
+    String homeUrl = "https://test.crowdstreet.com/invexp/properties/all";
+    HomePage homePage;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Users/tetianakravchuk/IdeaProjects/com.test.crowdstreet/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(homeUrl);
+        driver.get("chrome://settings/clearBrowserData");
+        driver.findElement(By.xpath("//settings-ui")).sendKeys(Keys.ENTER);
+        driver.manage().window().maximize();
+        driver.get("https://test.crowdstreet.com/invexp/properties/all");
+        homePage = new HomePage(driver, wait);
+
+    }
+
+    @AfterMethod
+    public void afterAction() {
+        driver.close();
+        driver.quit();
+    }
+}
